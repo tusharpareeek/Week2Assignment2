cc6b78f41788e4905c33895def2064a40e2a1b17
Tushar Pareek <tusharpareek1302@gmail.com>
2024-09-12 15:43:23 +0530

    Week2Assignment2_Constants.java

--- Week2Assignment2_Constants.java
+++ Week2Assignment2_Constants.java
@@ -1,3 +1,10 @@
+/**
+* Week 2 Assignment Task 2: Advanced Java Programming Assignment
+* Constant class for this assignment
+* 
+* @author Tushar Pareek
+* @since  11/09/2024
+*/
 public class Week2Assignment2_Constants {
     public final String task = "\n From the following options which program would you like to run:\n 1. Count Palindrome\n 2. Find Nth Fibonacci Series\n 3. Convert snake_case to camelCase\n 4. Count consonants in a string\n 5. Convert Binary number to Decimal number";
     public final String enterString = "Please enter a string:";
